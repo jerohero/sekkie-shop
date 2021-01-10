e7788fe78a45081d4cad7f52b247f99f962e0cef
Jeroen <jegbol01@gmail.com>
2021-01-10 15:08:34 +0100

    Added service for validating users

---
+++ frontend/src/app/shared/validate.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ValidateService {
+
+  constructor() { }
+
+  validateRegister(user) {
+    return !(user.name === undefined || user.email === undefined);
+  }
+
+  validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+}
