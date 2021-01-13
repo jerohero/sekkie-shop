0a3f2c95b68bd5b753657e31b5a7057741b1fd49
Jeroen <jegbol01@gmail.com>
2021-01-13 16:58:50 +0100

    Shopping cart is stored in local storage

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ShoppingListService} from './shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'item';
   showLogin: boolean;
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  ngOnInit(): void {
+    this.shoppingListService.fetchLocalStorageCart();
+  }
 
   onNavigate(feature: string): void {
     this.loadedFeature = feature;
--- frontend/src/app/shopping-list/shopping-list.service.ts
+++ frontend/src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 
 import { Item } from '../shared/item.model';
 import { Subject } from 'rxjs';
@@ -9,8 +9,20 @@ export class ShoppingListService {
   shoppingCartChanged = new Subject<ShoppingCartItem[]>();
   private shoppingCartItems: ShoppingCartItem[] = [];
 
+  fetchLocalStorageCart(): void {
+    const localStorageItems = localStorage.getItem('cart');
+    if (localStorageItems) {
+      this.setItems(JSON.parse(localStorageItems));
+    }
+  }
+
   getItems() {
     return this.shoppingCartItems.slice();
+  }
+
+  setItems(items): void {
+    this.shoppingCartItems = items;
+    this.shoppingCartChanged.next(this.shoppingCartItems.slice());
   }
 
   addItem(item: Item, {selectedSize, selectedColor}) {
@@ -25,5 +37,6 @@ export class ShoppingListService {
       1);
     this.shoppingCartItems.push(shoppingCartItem);
     this.shoppingCartChanged.next(this.shoppingCartItems.slice());
+    localStorage.setItem('cart', JSON.stringify(this.shoppingCartItems));
   }
 }
