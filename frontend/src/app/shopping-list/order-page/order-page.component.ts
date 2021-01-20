a19c3538e4bb10c348b7a6407ba04d4e8451249f
Jeroen <jegbol01@gmail.com>
2021-01-20 12:24:33 +0100

    Initial styling

--- frontend/src/app/shopping-list/order-page/order-page.component.ts
+++ frontend/src/app/shopping-list/order-page/order-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {DataStorageService} from '../../shared/services/data-storage.service';
+import {User} from '../../shared/models/user.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.scss']
 })
 export class OrderPageComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit(): void {
+    if (this.dataStorageService.user) {
+      this.user = this.dataStorageService.user.getValue();
+    }
   }
 
+  onContinue(accountInfoForm: NgForm) {
+
+  }
 }
--- frontend/src/app/shopping-list/order-page/order.service.ts
+++ frontend/src/app/shopping-list/order-page/order.service.ts
@@ -7,4 +7,6 @@ export class OrderService {
 
   constructor() {
   }
+
+
 }
