93f7bc2d4ff7a2d88ddd9d5e3fb5433037388097
Jeroen <jegbol01@gmail.com>
2021-01-23 23:27:06 +0100

    Auth guard now checks for token instead of logged in user

--- frontend/src/app/shared/guard/auth.guard.ts
+++ frontend/src/app/shared/guard/auth.guard.ts
@@ -2,7 +2,6 @@ import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTre
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {DataStorageService} from '../services/data-storage.service';
-import {map} from 'rxjs/operators';
 import {AuthService} from '../../login/auth.service';
 
 @Injectable({providedIn: 'root'})
@@ -14,14 +13,21 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot
   ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
-    return this.dataStorageService.user
-      .pipe(map((user) => {
-        const isAuth = !!user;
-        if (isAuth) {
-          return true;
-        }
-        this.authService.showLogin.next(true);
-        return this.router.createUrlTree(['/']);
-      }));
+    const token = localStorage.getItem('token');
+    const isAuth = !!token;
+    if (isAuth) {
+      return true;
+    }
+    this.authService.showLogin.next(true);
+    return this.router.createUrlTree(['/']);
+    // return this.dataStorageService.user
+    //   .pipe(map((user) => {
+    //     const isAuth = !!user;
+    //     if (isAuth) {
+    //       return true;
+    //     }
+    //     this.authService.showLogin.next(true);
+    //     return this.router.createUrlTree(['/']);
+    //   }));
   }
 }
